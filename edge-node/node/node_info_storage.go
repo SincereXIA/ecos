960de77657cad112f323cd33df58647ffa4de5fe
Zhang Junhua <zhangjh@act.buaa.edu.cn>
2022-02-12 11:11:02 +0800

    NodeInfoStorage support term

--- edge-node/node/node_info_storage.go
+++ edge-node/node/node_info_storage.go
@@ -2,45 +2,111 @@ package node
 
 import (
 	"errors"
+	"github.com/mohae/deepcopy"
+	"time"
 )
 
-type NodeID uint64
+type ID uint64
 
-type NodeInfoStorage interface {
+type InfoStorage interface {
 	UpdateNodeInfo(info *NodeInfo) error
-	DeleteNodeInfo(nodeId NodeID) error
-	GetNodeInfo(nodeId NodeID) (*NodeInfo, error)
-	ListAllNodeInfo() map[NodeID]NodeInfo
-	// GetGroupInfo() sun.GroupInfo
+	DeleteNodeInfo(nodeId ID) error
+	GetNodeInfo(nodeId ID) (*NodeInfo, error)
+	SetLeader(nodeId ID) error
+	// Commit 正式启用缓存区的节点信息
+	Commit()
+
+	// ListAllNodeInfo 用于节点间通信时获取节点信息
+	ListAllNodeInfo() map[ID]NodeInfo
+	// GetGroupInfo 用于为 Crush 算法提供集群信息
+	GetGroupInfo() *GroupInfo
 }
 
 type MemoryNodeInfoStorage struct {
-	infoMap map[NodeID]NodeInfo
+	uncommittedInfoMap   map[ID]NodeInfo
+	nowInfoMap           map[ID]NodeInfo
+	nowGroupInfo         *GroupInfo
+	uncommittedGroupInfo *GroupInfo
 }
 
 func (storage *MemoryNodeInfoStorage) UpdateNodeInfo(info *NodeInfo) error {
 	nodeId := info.RaftId
-	storage.infoMap[NodeID(nodeId)] = *info
+	storage.uncommittedInfoMap[ID(nodeId)] = *info
+	storage.updateTimestamp()
 	return nil
 }
 
-func (storage *MemoryNodeInfoStorage) DeleteNodeInfo(nodeId NodeID) error {
-	delete(storage.infoMap, nodeId)
+func (storage *MemoryNodeInfoStorage) DeleteNodeInfo(nodeId ID) error {
+	delete(storage.uncommittedInfoMap, nodeId)
+	storage.updateTimestamp()
 	return nil
 }
 
-func (storage *MemoryNodeInfoStorage) GetNodeInfo(nodeId NodeID) (*NodeInfo, error) {
-	if nodeInfo, ok := storage.infoMap[nodeId]; ok {
+func (storage *MemoryNodeInfoStorage) GetNodeInfo(nodeId ID) (*NodeInfo, error) {
+	if nodeInfo, ok := storage.uncommittedInfoMap[nodeId]; ok {
 		return &nodeInfo, nil
 	}
 	return nil, errors.New("node not found")
 
 }
 
-func (storage *MemoryNodeInfoStorage) ListAllNodeInfo() map[NodeID]NodeInfo {
-	return storage.infoMap
+func (storage *MemoryNodeInfoStorage) ListAllNodeInfo() map[ID]NodeInfo {
+	return storage.uncommittedInfoMap
+}
+
+func (storage *MemoryNodeInfoStorage) Commit() {
+	storage.nowInfoMap = storage.uncommittedInfoMap
+	cpy := deepcopy.Copy(storage.uncommittedGroupInfo)
+	storage.nowGroupInfo = cpy.(*GroupInfo)
+	storage.nowGroupInfo.NodesInfo = map2Slice(storage.nowInfoMap)
+	oldTerm := storage.uncommittedGroupInfo.Term
+	storage.uncommittedGroupInfo.Term = uint64(time.Now().UnixNano())
+	// prevent commit too quick let term equal
+	if storage.uncommittedGroupInfo.Term == oldTerm {
+		storage.uncommittedGroupInfo.Term += 1
+	}
+}
+
+func (storage *MemoryNodeInfoStorage) GetGroupInfo() *GroupInfo {
+	return storage.nowGroupInfo
+}
+
+func (storage *MemoryNodeInfoStorage) SetLeader(nodeId ID) error {
+	if info, ok := storage.uncommittedInfoMap[nodeId]; ok {
+		storage.uncommittedGroupInfo.LeaderInfo = &info
+		storage.updateTimestamp()
+	} else {
+		return errors.New("leader not found")
+	}
+	return nil
 }
 
 func NewMemoryNodeInfoStorage() *MemoryNodeInfoStorage {
-	return &MemoryNodeInfoStorage{infoMap: map[NodeID]NodeInfo{}}
+	return &MemoryNodeInfoStorage{
+		uncommittedInfoMap: make(map[ID]NodeInfo),
+		nowInfoMap:         make(map[ID]NodeInfo),
+		nowGroupInfo: &GroupInfo{
+			Term:            0,
+			LeaderInfo:      nil,
+			NodesInfo:       nil,
+			UpdateTimestamp: uint64(time.Now().UnixNano()),
+		},
+		uncommittedGroupInfo: &GroupInfo{
+			Term:            uint64(time.Now().UnixNano()),
+			LeaderInfo:      nil,
+			NodesInfo:       nil,
+			UpdateTimestamp: uint64(time.Now().UnixNano()),
+		},
+	}
+}
+
+func map2Slice(input map[ID]NodeInfo) (output []*NodeInfo) {
+	for _, value := range input {
+		output = append(output, &value)
+	}
+	return
+}
+
+func (storage *MemoryNodeInfoStorage) updateTimestamp() {
+	storage.uncommittedGroupInfo.UpdateTimestamp = uint64(time.Now().Unix())
 }
--- go.mod
+++ go.mod
@@ -13,6 +13,7 @@ require (
 	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/mitchellh/copystructure v1.2.0
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/mohae/deepcopy v0.0.0-20170929034955-c48cc78d4826
 	github.com/sirupsen/logrus v1.8.1
 	github.com/urfave/cli/v2 v2.3.0
 	go.etcd.io/etcd v2.3.8+incompatible
