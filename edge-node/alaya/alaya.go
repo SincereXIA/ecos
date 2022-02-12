e57bf669f2c05b6bb08ceb7f8cd6ac577b252bf6
Zhang Junhua <zhangjh@act.buaa.edu.cn>
2022-02-12 19:40:21 +0800

    #8: Define BlockData & Meta in Gaia & Alaya

---
+++ edge-node/alaya/alaya.go
@@ -0,0 +1,25 @@
+package alaya
+
+import (
+	"context"
+	"ecos/messenger"
+	"ecos/messenger/common"
+)
+
+// Alaya process record & inquire object Mata request
+// 阿赖耶处理对象元数据的存储和查询请求
+// 一切众生阿赖耶识，本来而有圆满清净，出过于世同于涅槃
+type Alaya struct {
+	UnimplementedAlayaServer
+}
+
+func (a *Alaya) RecordObjectMeta(context.Context, *ObjectMeta) (*common.Result, error) {
+	// TODO: 处理收到的元数据，转发给同组 Node
+	return nil, nil
+}
+
+func NewAlaya(rpcServer *messenger.RpcServer) *Alaya {
+	a := Alaya{}
+	RegisterAlayaServer(rpcServer, &a)
+	return &a
+}
---
+++ edge-node/gaia/gaia.go
@@ -0,0 +1,39 @@
+package gaia
+
+import (
+	"ecos/messenger"
+	"ecos/messenger/common"
+	"ecos/utils/logger"
+	"io"
+)
+
+// Gaia save object block data
+// 盖亚存储和查询对象 block 数据
+// 地势坤，厚德载物
+type Gaia struct {
+	UnimplementedGaiaServer
+}
+
+func (g *Gaia) UploadBlockData(stream Gaia_UploadBlockDataServer) error {
+
+	for {
+		r, err := stream.Recv()
+		if err == io.EOF {
+			return stream.SendAndClose(&common.Result{
+				Status: common.Result_OK,
+			})
+		}
+		if err != nil {
+			return err
+		}
+		logger.Infof("receive size: %v", len(r.Content))
+		// TODO: 处理接收到的 Block，转发给同组 Node
+	}
+	return nil
+}
+
+func NewGaia(rpcServer *messenger.RpcServer) *Gaia {
+	g := Gaia{}
+	RegisterGaiaServer(rpcServer, &g)
+	return &g
+}
