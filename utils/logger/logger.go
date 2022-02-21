13cb0ad0e78b6631b46afe6123f7df995f536422
sincerexia <zhjunhua98@gmail.com>
2022-02-21 10:46:45 +0800

    logger support set loglevel

--- utils/logger/logger.go
+++ utils/logger/logger.go
@@ -1,12 +1,40 @@
 package logger
 
-import log "github.com/sirupsen/logrus"
+import (
+	log "github.com/sirupsen/logrus"
+)
 
 type Logger struct {
 }
 
+type LogLevel int
+
+const (
+	TraceLevel LogLevel = iota
+	DebugLevel
+	InfoLevel
+)
+
+func SetLogLevel(level LogLevel) {
+	switch level {
+	case TraceLevel:
+		log.SetLevel(log.TraceLevel)
+		break
+	case DebugLevel:
+		log.SetLevel(log.DebugLevel)
+		break
+	case InfoLevel:
+		log.SetLevel(log.InfoLevel)
+		break
+	}
+}
+
 func Tracef(format string, args ...interface{}) {
 	log.Tracef(format, args...)
+}
+
+func Debugf(format string, args ...interface{}) {
+	log.Debugf(format, args...)
 }
 
 func Infof(format string, args ...interface{}) {
