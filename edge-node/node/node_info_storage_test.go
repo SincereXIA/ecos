7cd94b636e2fe639c373932635af8f65fed3c3a6
qiutb <qiutb@act.buaa.edu.cn>
2022-03-15 18:58:02 +0800

    implement StableNodeInfoStorage

--- edge-node/node/node_info_storage_test.go
+++ edge-node/node/node_info_storage_test.go
@@ -17,7 +17,7 @@ func TestMemoryNodeInfoStorage(t *testing.T) {
 	_ = common.InitAndClearPath(testDataBaseDir)
 	stableStorage := NewStableNodeInfoStorage(testDataBaseDir)
 	defer stableStorage.Close()
-	testStableStorage(stableStorage, t)
+	testStorage(stableStorage, t)
 	_ = os.RemoveAll(testDataBaseDir)
 }
 
@@ -60,8 +60,8 @@ func testStorage(storage InfoStorage, t *testing.T) {
 	t.Run("test info commit", func(t *testing.T) {
 		storage.Commit(uint64(time.Now().UnixNano()))
 		// TODO:  rocksdb 的 infoStorage 完成后，取消下面注释
-		//group := storage.GetGroupInfo()
-		//assert.Empty(t, group.NodesInfo)
+		// group := storage.GetGroupInfo(0)
+		// assert.Empty(t, group.NodesInfo)
 	})
 	t.Run("test info apply", func(t *testing.T) {
 		storage.Apply()
@@ -91,81 +91,5 @@ func testStorage(storage InfoStorage, t *testing.T) {
 	})
 	t.Run("test hook", func(t *testing.T) {
 		assert.Equal(t, 3, hookInfoNum)
-	})
-}
-
-func testStableStorage(storage *StableNodeInfoStorage, t *testing.T) {
-	hookInfoNum := 0
-	t.Run("test init", func(t *testing.T) {
-		groupInfo := storage.GetGroupInfo(0)
-		assert.Equal(t, groupInfo.GroupTerm.Term, uint64(0))
-		_, err := storage.GetNodeInfo(0)
-		assert.NotNil(t, err)
-		storage.SetOnGroupApply(func(info *GroupInfo) {
-			hookInfoNum = len(info.NodesInfo)
-		})
-	})
-	t.Run("test add node info", func(t *testing.T) {
-		info1 := NodeInfo{
-			RaftId:   1,
-			Uuid:     uuid.New().String(),
-			IpAddr:   "8.8.8.8",
-			RpcPort:  8888,
-			Capacity: 8888,
-		}
-		info2 := NodeInfo{
-			RaftId:   2,
-			Uuid:     uuid.New().String(),
-			IpAddr:   "8.8.8.8",
-			RpcPort:  8888,
-			Capacity: 8888,
-		}
-		assert.NoError(t, storage.UpdateNodeInfo(&info1))
-		assert.NoError(t, storage.UpdateNodeInfo(&info2))
-		infos := storage.ListAllNodeInfo()
-		assert.Equal(t, len(infos), 2)
-		group := storage.GetGroupInfo(0)
-		assert.Empty(t, group.NodesInfo)
-	})
-	t.Run("test update leader info", func(t *testing.T) {
-		assert.NoError(t, storage.SetLeader(1))
-	})
-	t.Run("test info commit", func(t *testing.T) {
-		storage.Commit(uint64(time.Now().UnixNano()))
-		// TODO:  rocksdb 的 infoStorage 完成后，取消下面注释
-		//group := storage.GetGroupInfo()
-		//assert.Empty(t, group.NodesInfo)
-	})
-	t.Run("test info apply", func(t *testing.T) {
-		storage.Apply()
-		group := storage.GetGroupInfo(0)
-		assert.NotEmpty(t, group.NodesInfo)
-		t.Logf("Old term: %v, new term: %v", 0, group.GroupTerm.Term)
-		assert.NotEqual(t, group.GroupTerm.Term, uint64(0))
-		term := group.GroupTerm.Term
-		info3 := NodeInfo{
-			RaftId:   3,
-			Uuid:     uuid.New().String(),
-			IpAddr:   "8.8.8.8",
-			RpcPort:  8888,
-			Capacity: 8888,
-		}
-		assert.NoError(t, storage.UpdateNodeInfo(&info3))
-		group = storage.GetGroupInfo(0)
-		assert.Equal(t, len(group.NodesInfo), 2)
-		storage.Commit(uint64(time.Now().UnixNano()))
-		// TODO:  rocksdb 的 infoStorage 完成后，取消下面注释
-		assert.Equal(t, len(group.NodesInfo), 2)
-		storage.Apply()
-		group = storage.GetGroupInfo(0)
-		t.Logf("Old term: %v, new term: %v", term, group.GroupTerm.Term)
-		assert.NotEqual(t, group.GroupTerm.Term, term)
-		assert.Equal(t, len(group.NodesInfo), 3)
-	})
-	t.Run("test hook", func(t *testing.T) {
-		assert.Equal(t, 3, hookInfoNum)
-	})
-	t.Run("test save memory info", func(t *testing.T) {
-		storage.SaveMemoryNodeInfoStorage()
 	})
 }
--- edge-node/node/stable_node_info_storage.go
+++ edge-node/node/stable_node_info_storage.go
@@ -30,46 +30,81 @@ func init() {
 	opts, readOptions, writeOptions = database.InitRocksdb()
 }
 
+func (s *StableNodeInfoStorage) UpdateNodeInfo(info *NodeInfo) error {
+	err := s.MemoryNodeInfoStorage.UpdateNodeInfo(info)
+	if err != nil {
+		return err
+	}
+	err = s.SaveMemoryNodeInfoStorage()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (s *StableNodeInfoStorage) DeleteNodeInfo(nodeId ID) error {
+	err := s.MemoryNodeInfoStorage.DeleteNodeInfo(nodeId)
+	if err != nil {
+		return err
+	}
+	err = s.SaveMemoryNodeInfoStorage()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
 // SaveMemoryNodeInfoStorage Save MemoryNodeInfoStorage
-func (s *StableNodeInfoStorage) SaveMemoryNodeInfoStorage() {
+func (s *StableNodeInfoStorage) SaveMemoryNodeInfoStorage() error {
+
 	historyProtoData, err := proto.Marshal(&s.history)
 	if err != nil {
 		logger.Errorf("Marshal history failed, err:%v", err)
+		return err
 	}
 	err = s.db.Put(writeOptions, []byte(history), historyProtoData)
 	if err != nil {
 		logger.Errorf("put history failed, err:%v", err)
+		return err
 	}
 
 	nowStateProtoData, err := proto.Marshal(s.nowState)
 	if err != nil {
 		logger.Errorf("Marshal nowState failed, err:%v", err)
+		return err
 	}
 	err = s.db.Put(writeOptions, []byte(nowState), nowStateProtoData)
 	if err != nil {
 		logger.Errorf("put nowState failed, err:%v", err)
+		return err
 	}
 
 	committedStateProtoData, err := proto.Marshal(s.committedState)
 	if err != nil {
 		logger.Errorf("Marshal committedState failed, err:%v", err)
+		return err
 	}
 	err = s.db.Put(writeOptions, []byte(committedState), committedStateProtoData)
 	if err != nil {
 		logger.Errorf("put committedState failed, err:%v", err)
+		return err
 	}
 
 	uncommittedStateProtoData, err := proto.Marshal(s.uncommittedState)
 	if err != nil {
 		logger.Errorf("Marshal uncommittedState failed, err:%v", err)
+		return err
 	}
 	err = s.db.Put(writeOptions, []byte(uncommittedState), uncommittedStateProtoData)
 	if err != nil {
 		logger.Errorf("put uncommittedState failed, err:%v", err)
+		return err
 	}
+	return nil
 }
 
-func (s *StableNodeInfoStorage) RecoverMemoryNodeInfoStorage() {
+func RecoverMemoryNodeInfoStorage() {
 	// TODO
 }
 
