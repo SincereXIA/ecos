98a81d4c5c9c560ad85522e23c6ed847d1ff5cd3
Zhang Junhua <zhangjh@act.buaa.edu.cn>
2022-04-21 09:45:45 +0800

    fix(monitor): fix test

--- edge-node/watcher/monitor_test.go
+++ edge-node/watcher/monitor_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"ecos/messenger"
 	"github.com/stretchr/testify/assert"
+	"os"
 	"testing"
 	"time"
 )
@@ -14,15 +15,13 @@ func TestMonitor(t *testing.T) {
 	defer cancel()
 	basePath := "./ecos-data/"
 	watchers, rpcServers, _ := GenTestWatcherCluster(ctx, basePath, nodeNum)
-	monitors := genTestMonitors(ctx, watchers, rpcServers)
-	for i, rpc := range rpcServers {
+	for _, rpc := range rpcServers {
 		go func(rpc *messenger.RpcServer) {
 			err := rpc.Run()
 			if err != nil {
 				t.Errorf("rpc server failed: %v", err)
 			}
 		}(rpc)
-		go monitors[i].Run()
 	}
 	RunAllTestWatcher(watchers)
 	WaitAllTestWatcherOK(watchers)
@@ -36,26 +35,18 @@ func TestMonitor(t *testing.T) {
 	}
 	assert.Greater(t, leader, -1)
 
-	for status := monitors[leader].GetAllReports(); len(status) != len(watchers); {
+	for status := watchers[leader].monitor.GetAllReports(); len(status) != len(watchers); {
 		time.Sleep(time.Second)
 	}
 
-	status := monitors[leader].GetAllReports()
+	status := watchers[leader].monitor.GetAllReports()
 	for _, s := range status {
 		t.Logf("%v", s)
 	}
 
-	t.Run("monitor stop", func(t *testing.T) {
-		c := monitors[leader].GetEventChannel()
-		monitors[nodeNum-1].Stop()
-		timer := time.NewTimer(time.Second * 5)
-		select {
-		case event := <-c:
-			t.Logf("get node %v stop event", event.Report.NodeId)
-			assert.Equal(t, uint64(nodeNum), event.Report.NodeId)
-		case <-timer.C:
-			t.Errorf("monitor not find node stopped")
-		}
+	t.Cleanup(func() {
+		cancel()
+		_ = os.RemoveAll(basePath)
 	})
 }
 
