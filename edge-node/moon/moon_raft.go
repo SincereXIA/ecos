0969f4dd87205792b2bae206e2a017b1d26c53e9
qiutb <qiutb@act.buaa.edu.cn>
2022-04-27 15:33:56 +0800

    perf(infos): optimize snapshot proto struct

--- edge-node/moon/moon_raft.go
+++ edge-node/moon/moon_raft.go
@@ -58,7 +58,7 @@ type raftNode struct {
 	logger *zap.Logger
 }
 
-var defaultSnapshotCount uint64 = 10000 // set 10 for test
+var defaultSnapshotCount uint64 = 10 // set 10 for test
 
 func newRaftNode(id int, ctx context.Context, peers []raft.Peer, join bool, basePath string, readyC chan bool, getSnapshot func() ([]byte, error)) (chan *snap.Snapshotter, *raftNode) {
 
@@ -310,7 +310,7 @@ func (rc *raftNode) publishSnapshot(snapshotToSave raftpb.Snapshot) {
 	rc.appliedIndex = snapshotToSave.Metadata.Index
 }
 
-var snapshotCatchUpEntriesN uint64 = 10000 // set 10 for test
+var snapshotCatchUpEntriesN uint64 = 10 // set 10 for test
 
 func (rc *raftNode) maybeTriggerSnapshot(applyDoneC <-chan struct{}) {
 	if rc.appliedIndex-rc.snapshotIndex <= rc.snapCount {
