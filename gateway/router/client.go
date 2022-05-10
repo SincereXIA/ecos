7ca2a2d1bc4a75adccf341ff578931fd7f377e76
Huchao Xiong <xionghc@act.buaa.edu.cn>
2022-05-10 20:55:50 +0800

    fix(gateway): fix unable to connect error
    Expose port 8080

--- gateway/router/client.go
+++ gateway/router/client.go
@@ -4,15 +4,30 @@ import (
 	"ecos/client"
 	"ecos/client/config"
 	"ecos/utils/logger"
+	"time"
 )
 
 var Client *client.Client
 
 func InitClient(config config.ClientConfig) {
-	c, err := client.New(&config)
-	if err != nil {
-		logger.Fatalf("Init client for gateway failed, err: %v", err)
+	var c *client.Client
+	var err error
+	timer := time.NewTimer(time.Minute * 5)
+	for {
+		select {
+		case <-timer.C:
+			return
+		default:
+		}
+		c, err = client.New(&config)
+		if err != nil {
+			logger.Warningf("Init client for gateway failed, err: %v", err)
+			time.Sleep(time.Second * 3)
+			logger.Infof("Retry init client for gateway")
+			continue
+		}
+		logger.Infof("Init client for gateway success")
+		break
 	}
-	logger.Warningf("Init client for gateway success")
 	Client = c
 }
