2444c614993a2a164e430b9015809fdac8bbe8b0
Zhang Junhua <zhangjh@act.buaa.edu.cn>
2022-10-01 09:18:32 +0800

    feat(edge-node): listen os signal for clean up
    监听系统信号，收到退出信号后先完成一些清理工作

--- edge-node/cmd/node.go
+++ edge-node/cmd/node.go
@@ -15,7 +15,11 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/spf13/cobra"
 	"net/http"
+	"os"
+	"os/signal"
 	"strconv"
+	"syscall"
+	"time"
 )
 
 var nodeCmd = &cobra.Command{
@@ -91,6 +95,18 @@ func nodeRun(cmd *cobra.Command, _ []string) {
 		_ = g.Run()
 	}()
 	logger.Infof("edge node init success")
+
+	// 监听系统信号
+	c := make(chan os.Signal)
+	signal.Notify(c, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+	go func() {
+		for s := range c {
+			logger.Infof("receive signal from os: %v, ECOS node start stop", s)
+			cancel()
+			time.Sleep(time.Second)
+			os.Exit(0)
+		}
+	}()
 
 	// init Gin
 	router := newRouter(w)
